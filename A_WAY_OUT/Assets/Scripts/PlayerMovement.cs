b6024f13418c7072e26935935ddceb862638a4a4
lucus0621 <lucus0621@gmail.com>
2022-03-03 12:10:14 -0800

    First Person camera

---
+++ A_WAY_OUT/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    public float jumpHeight = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
