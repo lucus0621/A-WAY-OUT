c7ff7041d13ea357b4971e416ac1136f19a4ce1f
lucus0621 <44933812+lucus0621@users.noreply.github.com>
2022-03-10 18:50:27 -0800

    SoundEffect

--- A_WAY_OUT/Assets/Scripts/DoorTrigger.cs
+++ A_WAY_OUT/Assets/Scripts/DoorTrigger.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    private Door m_Door;
     private bool enterCollide = false;
+
+    public GameObject Door;
+    public bool Door_false = false;
+    Quaternion targetAngels01;
+
+    public AudioSource DoorOpen;
+    public bool IsOpenClose = false;
     // Start is called before the first frame update
     void Start()
     {
-        m_Door = GameObject.Find("Doorshaft").GetComponent<Door>();
+
     }
 
 
@@ -27,18 +33,35 @@ public class DoorTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Door_false == true)
+        {
+
+            Door.transform.rotation = Quaternion.Slerp(Door.transform.rotation, targetAngels01, 1 * Time.deltaTime);
+
+            if (Quaternion.Angle(targetAngels01, Door.transform.rotation) < 1)
+            {
+                Door.transform.rotation = targetAngels01;
+                Door_false = false;
+            }
+
+        }
+
         if (enterCollide)
         {
             if (Input.GetKeyDown(KeyCode.F))
             {
-                if (m_Door.GetIsOpen())
-                {
-                    m_Door.CloseDoorMethod();
+                DoorOpen.Play();
+                Door_false = true;
 
+                if (IsOpenClose == false)
+                {
+                    targetAngels01 = Quaternion.Euler(0, -90, 0);
+                    IsOpenClose = true;
                 }
                 else
                 {
-                    m_Door.OpenDoorMethod();
+                    targetAngels01 = Quaternion.Euler(0, 0, 0);
+                    IsOpenClose = false;
                 }
             }
         }
