c42d731a56403f80ccf94f1ca2784782ae8155be
lucus0621 <44933812+lucus0621@users.noreply.github.com>
2022-03-12 18:52:26 -0800

    TransformHideWay

---
+++ A_WAY_OUT/Assets/Scripts/HideWayToGate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideWayToGate : MonoBehaviour
+{
+    public Transform MarkPos;
+    private GameObject gameObjectsl;
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("HideWay");
+        gameObjectsl = collider.gameObject;
+        collider.gameObject.SetActive(false);
+        collider.transform.position = MarkPos.position;
+        Invoke("ShowObj", 0.5f);
+    }
+
+
+    void ShowObj()
+    {
+        CancelInvoke();
+        gameObjectsl.SetActive(true);
+    }
+}
