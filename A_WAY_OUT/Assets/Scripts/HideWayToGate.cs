eba70666d8c465c7b38586c7d68e8a8e54675cb6
lucus0621 <44933812+lucus0621@users.noreply.github.com>
2022-03-15 22:32:51 -0700

    .

--- A_WAY_OUT/Assets/Scripts/HideWayToGate.cs
+++ A_WAY_OUT/Assets/Scripts/HideWayToGate.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class HideWayToGate : MonoBehaviour
 {
+    public static bool key1 = false;
     public AudioSource HideWayBroken;
     public Transform MarkPos;
     private GameObject gameObjectsl;
+
+    private void Start()
+    {
+        key1 = false;
+    }
+
     void OnTriggerEnter(Collider collider)
     {
-        HideWayBroken.Play();
-        Debug.Log("HideWay");
-        gameObjectsl = collider.gameObject;
-        collider.gameObject.SetActive(false);
-        collider.transform.position = MarkPos.position;
-        Invoke("ShowObj", 0.5f);
+        if (key1)
+        {
+            HideWayBroken.Play();
+            Debug.Log("HideWay");
+            gameObjectsl = collider.gameObject;
+            collider.gameObject.SetActive(false);
+            collider.transform.position = MarkPos.position;
+            Invoke("ShowObj", 0.5f);
+        }
     }
 
 
